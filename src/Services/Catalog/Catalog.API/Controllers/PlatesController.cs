17b84ebd78af9fc3048ef29a946bc3c06efebac1
danielschoder <daniel@schoder.uk>
2024-10-23 22:35:29 +0100

    RT-3

---
+++ src/Services/Catalog/Catalog.API/Controllers/PlatesController.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.BLL;
+using Catalog.API.DTOs;
+
+namespace Catalog.API.Controllers;
+
+[ApiController]
+[Route("api/gates")]
+public class PlatesController : Controller
+{
+    private readonly IPlatesManager _platesManager;
+
+    public PlatesController(IPlatesManager platesManager)
+    {
+        _platesManager = platesManager;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PlateDto>>> GetPlates()
+    {
+        return Ok(await _platesManager.GetPlatesAsync());
+    }
+}
